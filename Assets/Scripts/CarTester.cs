f9bc609e8b62e67781b65d6cf6471f1cd0de6a5c
Ecxxlipse <arnold219@morrisville.edu>
2026-08-31 15:23:08 -0400

    Hierarchy, Prefab, and Script Finish

---
+++ Assets/Scripts/CarTester.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CarTester : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("My awesome car is ready! Hello from " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
