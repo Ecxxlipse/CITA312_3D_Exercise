4ac8a2de0f634c8bebcc4f2805172384ba91b260
Ecxxlipse <arnold219@morrisville.edu>
2026-09-07 13:03:16 -0400

    Exercise 3
    Added movement to the car (WASD movement) with messages displaying when the car is moving forward and backwards.

--- Assets/Scripts/CarTester.cs
+++ Assets/Scripts/CarTester.cs
@@ -1,16 +1,60 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarTester : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float turnSpeed = 10f;
+
+    public InputAction moveforward;
+    public InputAction turn;
     void Start()
     {
         Debug.Log("My awesome car is ready! Hello from " + gameObject.name);
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        moveforward.Enable();
+        turn.Enable();
+
+    }
+        void OnDisable()
+    {
+        moveforward.Disable();
+        turn.Disable();
+
+    }
     void Update()
     {
+        MoveVehicle();
+        TurnVehicle();
+
+    }
+    
+    void MoveVehicle()
+    {
+        float movement = moveforward.ReadValue<float>();
+
+        //If vehicle is moving Forward, it will display a message
+        if (movement != 0)
+        {
+            Debug.Log("Vehicle is Accelerating.");
+        }
+
+        //If vehicle is moving Backwards, it will display a message
+        if (movement < 0)
+        {
+        Debug.Log("Vehicle is reversing.");
+        }
         
+        transform.Translate(Vector3.forward * movement * moveSpeed * Time.deltaTime);
+    }
+
+    void TurnVehicle()
+    {
+        float turning = turn.ReadValue<float>();
+        transform.Rotate(Vector3.up * turning * turnSpeed * Time.deltaTime);
     }
 }
